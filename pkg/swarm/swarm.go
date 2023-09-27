f839f9e801a4f3d28917ce6d75151439591d7740
Haseeb Raja <has33braja@gmail.com>
2023-09-27 15:36:25 +0500

    chore: readded node health func

--- pkg/swarm/swarm.go
+++ pkg/swarm/swarm.go
@@ -1,12 +1,39 @@
 package swarm
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 type swarm struct {
 	addr string
 }
 
 func (s swarm) CheckNodeHealthAndReadiness() error {
-	// TODO implement me
-	panic("implement me")
+	url := "http://localhost:1635/health"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(err)
+	}
+	m := make(map[string]string)
+	_ = json.Unmarshal(body, &m)
+	if m["status"] == "ok" {
+		fmt.Println("is ok")
+	}
+	return nil
 }
 
 func (s swarm) BuyPostageStamp() error {
